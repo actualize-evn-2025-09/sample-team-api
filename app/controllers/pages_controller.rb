a69ffa347f36d57dc8552605088c4a3dc87cbb5e
jjrosen <joshrosen281@gmail.com>
2025-12-15 18:50:45 -0600

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    render json: { message: "Hello Class, this is Josh, the best lokking person in the class" }
+    render json: { message: "Hello Class, this is Josh, the best looking person in the class" }
   end
 end
