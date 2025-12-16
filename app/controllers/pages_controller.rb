041323ba1d1e4f70134d0dbb3e75404214a0fca9
jjrosen <joshrosen281@gmail.com>
2025-12-15 18:47:09 -0600

    added a personal touch to the message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    render json: { message: "Hello Class" }
+    render json: { message: "Hello Class, this is Josh, the best lokking person in the class" }
   end
 end
